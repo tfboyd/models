88c01965fc61c582d6970348283f5f3fee2c38f5
Anjali Sridhar <anjalisridhar@anjalisridhar.mtv.corp.google.com>
2018-10-17 00:02:35 -0700

    get the first weight

--- official/resnet/resnet_run_loop.py
+++ official/resnet/resnet_run_loop.py
@@ -278,7 +278,7 @@ def resnet_model_fn(features, labels, mode, model_class,
           tf.identity(l.moving_variance, 'bn_conv1_moving_variance')
         if 'res5c_branch2c' in l.name:
           # print("\n\n res5c_branch2c weights ", l.get_weights()[0][:5])
-          tf.identity(l.trainable_variables[0], 'conv1_training_weights')
+          tf.identity(l.trainable_variables[0][:1], 'conv1_training_weights')
     if mode != tf.estimator.ModeKeys.TRAIN:
       for l in model.layers:
         if 'bn5c_branch2a' in l.name:
@@ -287,7 +287,7 @@ def resnet_model_fn(features, labels, mode, model_class,
           tf.identity(l.moving_variance, 'bn_conv1_moving_variance_eval')
         if 'res5c_branch2c' in l.name:
           # print("\n\n res5c_branch2c weights ", l.get_weights()[0][:5])
-          tf.identity(l.trainable_variables[0], 'conv1_training_weights_eval')
+          tf.identity(l.trainable_variables[0][:1], 'conv1_training_weights_eval')
 
   # This acts as a no-op if the logits are already in fp32 (provided logits are
   # not a SparseTensor). If dtype is is low precision, logits must be cast to
@@ -571,7 +571,8 @@ def resnet_main(
     # TODO(anjalisridhar): Not evaluating
     tensors_to_log = dict((x, x) for x in [
                                         'bn_conv1_moving_mean_eval',
-                                        'bn_conv1_moving_variance_eval'])
+                                        'bn_conv1_moving_variance_eval',
+                                        'conv1_training_weights_evals'])
     eval_hooks = [tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=1)]
     
     eval_results = classifier.evaluate(input_fn=input_fn_eval,
