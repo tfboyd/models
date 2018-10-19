637fa7782318985ff390be4ceea7be8f2481d8e0
Anjali Sridhar <anjalisridhar@anjalisridhar.mtv.corp.google.com>
2018-10-19 15:53:16 -0700

    always set training to be true

--- official/resnet/resnet_run_loop.py
+++ official/resnet/resnet_run_loop.py
@@ -262,7 +262,7 @@ def resnet_model_fn(features, labels, mode, model_class,
   if use_keras_model:
     print("Using Keras ResNet50 1.5 model.")
     model = keras_resnet_model.ResNet50(classes=num_classes, weights=None,
-                                        training=(mode == tf.estimator.ModeKeys.TRAIN))
+                                        training=True)
     # print("\n\n model summary ", model.summary())
   else:
     model = model_class(resnet_size, data_format, resnet_version=resnet_version,
