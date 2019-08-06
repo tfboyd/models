4f02d096a2b4b0c455b4ad17d0155a012d1a48fc
Toby Boyd <tobyboyd@google.com>
2019-08-05 20:52:19 -0700

    move to benchmark

--- official/staging/shakespeare/shakespeare_benchmark.py
+++ official/staging/shakespeare/shakespeare_benchmark.py
@@ -194,17 +194,6 @@ class ShakespeareAccuracy(ShakespeareBenchmarkBase):
     FLAGS.enable_xla = True
     self._run_and_report_benchmark()
 
-  def benchmark_xla_1_gpu_bs_8(self):
-    """Benchmark 1 gpu w/xla."""
-    self._setup()
-    FLAGS.num_gpus = 1
-    FLAGS.training_data = self.train_data
-    FLAGS.batch_size = 8
-    FLAGS.train_epochs = 43
-    FLAGS.model_dir = ''
-    FLAGS.enable_xla = True
-    self._run_and_report_benchmark()
-
   def benchmark_8_gpu(self):
     """Benchmark 8 gpu.
 
@@ -331,6 +320,14 @@ class ShakespeareKerasBenchmarkReal(ShakespeareBenchmarkBase):
     self._setup()
     FLAGS.num_gpus = 1
     FLAGS.batch_size = 64
+    FLAGS.enable_xla = True
+    self._run_and_report_benchmark()
+
+  def benchmark_xla_1_gpu_bs_8(self):
+    """Benchmark 1 gpu w/xla."""
+    self._setup()
+    FLAGS.num_gpus = 1
+    FLAGS.batch_size = 8
     FLAGS.enable_xla = True
     self._run_and_report_benchmark()
 
